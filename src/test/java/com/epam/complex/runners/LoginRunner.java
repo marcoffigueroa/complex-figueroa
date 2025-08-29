cfc3c7417d32207a8b2c0c6072c8ab5b19393410
MarcoFigueroa <marco_figueroa@epam.com>
2025-08-29 13:26:46 -0300

    Sure fire added

--- src/test/java/com/epam/complex/runners/LoginRunner.java
+++ src/test/java/com/epam/complex/runners/LoginRunner.java
@@ -8,7 +8,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @IncludeEngines("cucumber")
 @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "classpath:")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.epam.complex.tests")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html, json:target/cucumber.json")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html, json:target/cucumber.json, summary")
 @IncludeTags("LoginTests")
 public class LoginRunner {
 }
